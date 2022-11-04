4e325e3cdcc75acd34e351ec4fd6c5d61857fb96
IvanBravoSanchez <114057614+IvanBravoSanchez@users.noreply.github.com>
2022-11-04 22:09:01 +0100

    practica

--- FranquiciaoMarca.R
+++ FranquiciaoMarca.R
@@ -16,9 +16,9 @@ data <- df_source %>% clean_names() %>% type_convert(locale = locale(decimal_mar
 
 #para saber si estas gasolineras son marcas o franquicias yo creo que no es posible con este dataset. Falta algun dato que te lo indique.
 #De todas formas, voy a cosiderar que las gasolineras con S.L. y S.A. son franquicias y las que no son marcas.
-data %<>% tidyr::extract(rotulo, c("extension"), "(S\\.L|S\\.A|\\bSL\\b|\\bSA\\b)", remove = F) %>% mutate(Franquicia = extension %in% c("SL", "SA", "S.A", "S.L"))
+data %<>% tidyr::extract(rotulo, c("extension"), "(S\\.L|S\\.A|\\bSL\\b|\\bSA\\b)", remove = F) %>% mutate(TIPO = extension %in% c("SL", "SA", "S.A", "S.L"))
 
-data$Franquicia <- plyr::mapvalues(data$Franquicia, from = c("TRUE", "FALSE"), to = c("Si", "No"), warn_missing = F)
+data$TIPO <- plyr::mapvalues(data$TIPO, from = c("TRUE", "FALSE"), to = c("FRANQUICIA", "MARCA"), warn_missing = F)
 data %>% view()
 
 
